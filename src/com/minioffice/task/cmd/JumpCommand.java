645ff3d7b7332fa305132fae202e02992069ad17
awol2010ex <awol2010ex@gmail.com>
2012-11-23 09:41:28 +0800

    使用论坛上的JumpCommand进行驳回，稍微修改了一下，貌似可以成功

---
+++ src/com/minioffice/task/cmd/JumpCommand.java
@@ -0,0 +1,92 @@
+package com.minioffice.task.cmd;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.activiti.engine.ActivitiException;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.impl.RepositoryServiceImpl;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+//跳转命令
+public class JumpCommand implements Command<Object>, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2372913338251310191L;
+	protected String executionId;//流程ID
+	protected String targetActivitiId;//跳转目标环节ID
+	protected String taskId;//当前任务ID
+	private Map<String, Object> variables;
+
+	public JumpCommand(String executionId, String targetActivitiId, String taskId ,
+			Map<String, Object> variables) {
+		this.executionId = executionId;
+		this.targetActivitiId = targetActivitiId;
+		this.variables = variables;
+		this.taskId=taskId;
+	}
+
+	public Object execute(CommandContext commandContext) {
+		if (executionId == null) {
+			throw new ActivitiException("executionId is null");
+		}
+		if (targetActivitiId == null) {
+			throw new ActivitiException("targetActivitiId is null!");
+		}
+
+		//查找运行中 的Execution
+		ExecutionEntity execution = commandContext.getExecutionManager()
+				.findExecutionById(executionId);
+		if (execution == null) {
+			HistoricProcessInstance hai = Context
+					.getProcessEngineConfiguration().getHistoryService()
+					.createHistoricProcessInstanceQuery()
+					.processInstanceId(executionId).singleResult();
+			if (hai != null)
+				throw new ActivitiException("execution " + executionId
+						+ " has been completed at " + hai.getEndTime());
+			else {
+				throw new ActivitiException("execution " + executionId
+						+ " doesn't exist");
+			}
+		}
+
+		//设置流程变量
+		if (variables != null && !variables.isEmpty()) {
+			execution.setVariables(variables);
+		}
+
+		//查找流程定义
+		String processDefinitionId = execution.getProcessDefinitionId();
+		RepositoryServiceImpl rsi = (RepositoryServiceImpl) Context
+				.getProcessEngineConfiguration().getRepositoryService();
+		ProcessDefinitionEntity pde = (ProcessDefinitionEntity) rsi
+				.getDeployedProcessDefinition(processDefinitionId);
+
+		ActivityImpl targetActiviti = pde.findActivity(targetActivitiId);
+		if (targetActiviti == null) {
+			throw new ActivitiException("targetActiviti " + targetActiviti
+					+ " doesn't exist");
+		}
+
+		//跳转
+		try {
+			execution.take(targetActiviti.getIncomingTransitions().iterator()
+					.next());
+			//删除当前任务
+			  Context.getCommandContext()
+	          .getTaskManager().deleteTask(taskId, false);
+			
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
--- src/com/minioffice/task/dwr/TaskDwr.java
+++ src/com/minioffice/task/dwr/TaskDwr.java
@@ -1,6 +1,6 @@
 package com.minioffice.task.dwr;
 
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,15 +9,8 @@ import net.sf.json.JSONObject;
 
 import org.activiti.engine.form.FormProperty;
 import org.activiti.engine.form.TaskFormData;
-import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.identity.User;
-import org.activiti.engine.impl.RepositoryServiceImpl;
-import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.impl.persistence.entity.TaskEntity;
-import org.activiti.engine.impl.pvm.process.ActivityImpl;
-import org.activiti.engine.impl.pvm.process.ProcessDefinitionImpl;
-import org.activiti.engine.impl.task.TaskDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.apache.shiro.SecurityUtils;
@@ -26,6 +19,8 @@ import org.directwebremoting.annotations.RemoteProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.minioffice.task.cmd.JumpCommand;
 
 //流程操作DWR
 @RemoteProxy
@@ -103,89 +98,20 @@ public class TaskDwr {
 			return false;
 		}
 
-		// 取得流程定义
-		ProcessDefinitionEntity processDefinition = null;
 		try {
-			processDefinition = (ProcessDefinitionEntity) ((RepositoryServiceImpl) processEngineFactoryBean
-					.getProcessEngineConfiguration().getRepositoryService())
-					.getDeployedProcessDefinition(task.getProcessDefinitionId());
+			// 跳转
+			processEngineFactoryBean
+					.getProcessEngineConfiguration()
+					.getCommandExecutorTxRequired()
+					.execute(
+							new JumpCommand(task.getProcessInstanceId(),
+									activityId, taskId,new HashMap<String, Object>()));
 		} catch (Exception e) {
 			logger.error("", e);
-		}
-		if (processDefinition == null) {
 			return false;
 		}
-		
-		// 更换其Execution
-				ExecutionEntity exeImpl = (ExecutionEntity) processInstance;
 
-				// 更换其活动的定义
-				ActivityImpl preActivity = ((ProcessDefinitionImpl) processDefinition)
-						.findActivity(activityId);
-				exeImpl.setActivity(preActivity);
-				exeImpl.setActive(true);
-				exeImpl.setBusinessKey(task.getProcessInstance().getBusinessKey());
-				exeImpl.setProcessDefinitionId(task.getProcessDefinitionId());
-				exeImpl.setProcessInstance(task.getProcessInstance());
-				exeImpl.setProcessDefinition(processDefinition);
-
-				//取得要驳回到的节点的任务定义
-				TaskDefinition taskDef = ((ProcessDefinitionEntity) processDefinition)
-						.getTaskDefinitions().get(preActivity.getId());
-				try {
-					TaskEntity newTask = (TaskEntity) processEngineFactoryBean
-							.getProcessEngineConfiguration().getTaskService()
-							.newTask();
-
-					newTask.setName((String)preActivity.getProperties().get("name"));//设置为要驳回得环节名称
-					newTask.setDescription(task.getDescription());
-					newTask.setExecution(exeImpl);
-					newTask.setCreateTime(new Date());
-
-					newTask.setExecutionId(task.getExecutionId());
-					newTask.setProcessInstanceId(task.getProcessInstanceId());
-					newTask.setProcessDefinitionId(task.getProcessDefinitionId());
-					newTask.setProcessInstance(task.getProcessInstance());
-
-					// 更换流程任务的定义
-					if (taskDef != null) {
-						newTask.setTaskDefinition(taskDef);
-					} else {
-						return false;
-					}
-
-					//保存新的任务
-					 processEngineFactoryBean
-						.getProcessEngineConfiguration().getTaskService().saveTask(newTask);
-					
-					
-					//加入之前节点被分派的人员
-					List<HistoricTaskInstance> targetTaskList =  processEngineFactoryBean
-							.getProcessEngineConfiguration()
-							.getHistoryService().createHistoricTaskInstanceQuery()
-							.processInstanceId(task.getProcessInstanceId())
-							.taskDefinitionKey(activityId).list();
-					if (targetTaskList != null && targetTaskList.size() > 0) {
-						for (HistoricTaskInstance ht : targetTaskList) {
-							 processEngineFactoryBean
-								.getProcessEngineConfiguration().getTaskService().addCandidateUser(
-									newTask.getId(), ht.getAssignee());
-						}
-					}
-					
-					
-					//完成当前任务
-					 processEngineFactoryBean
-						.getProcessEngineConfiguration().getTaskService().deleteTask(task.getId());
-
-				} catch (Exception e) {
-					logger.error("", e);
-
-					return false;
-				}
-
-				return true;
-
+		return true;
 	}
 
 }
