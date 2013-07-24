e79d42b467cc0c3fc2797c7fdb74b27cd8ce4082
awol2010ex <awol2010ex@gmail.com>
2013-07-24 11:09:43 +0800

    菜单初始化SQL

--- db/mysql/init.sql
+++ db/mysql/init.sql
@@ -1,5 +1,8 @@
 DROP TABLE t_biz_menu;
 CREATE TABLE t_biz_menu ( id varchar(255) NOT NULL, menu_index int, menu_name varchar(255), menu_url varchar(255), pid varchar(255), PRIMARY KEY (id), CONSTRAINT FK2EAF87636C6444C FOREIGN KEY (pid) REFERENCES t_biz_menu (id), INDEX FK2EAF87636C6444C (pid) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be0004004f407014004f407790000', 2, '用户管理', '/views/user/userList.jsp', null);
+insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be000400a8aff01400a8aff4b0000', 1, '我的正在运行流程', '/views/process/myProcessList.jsp', '1068a7be0163fa8ce95013fa8cf93bc0001');
+insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be000400a988201400a9882080000', 2, '流程监控', '/views/process/allProcessList.jsp', '1068a7be0163fa8ce95013fa8cf93bc0001');
 insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be0163fa8ce95013fa8ce95a50000', 1, '菜单管理', '/views/menu/menuTree.jsp', null);
 insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be0163fa8ce95013fa8cf93bc0001', 2, '流程管理', '', null);
 insert into t_biz_menu (id, menu_index, menu_name, menu_url, pid) values ('1068a7be0163fa8ce95013fa8d04f9a0002', 1, '流程模版列表', '/views/process/deploymentList.jsp', '1068a7be0163fa8ce95013fa8cf93bc0001');
